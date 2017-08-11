821de2387d425a9c466863a052326eee3432e623
Lingesin_Deng <abc12345@@>
2017-08-11 17:43:29 +0800

    URL i18n

--- WebPortal/production/common/commonCtrl.js
+++ WebPortal/production/common/commonCtrl.js
@@ -11,25 +11,40 @@ app.controller('headerCtrl', function ($scope, $state) {
 
 app.controller('footerCtrl', function ($scope, $state) {
     $scope.langs = [
-	    { lang: "中文 - 繁體", langKey: "zh-tw" },
-	    { lang: "中文 - 简体", langKey: "zh-cn" },
-	    { lang: "English - US", langKey: "en-us" }
+	    { lang: '中文 - 繁體', langKey: 'zh-tw' },
+	    { lang: '中文 - 简体', langKey: 'zh-cn' },
+	    { lang: 'English - US', langKey: 'en-us' }
     ];
+
+    $scope.langSelected = function (v) {
+        console.log(v);
+        var currentUrlHash = '' + window.location.hash;
+        var rout = currentUrlHash.replace(/#+\/+([a-zA-Z0-9_\-])+\//, '.');
+        console.log(currentUrlHash);
+        console.log(rout);
+        if (currentUrlHash == '' || currentUrlHash == null) {
+            v = 'zh-tw' + rout;
+        } else {
+            v += rout;
+        }
+        console.log(v);
+        $state.go(v);
+    };
 });
 
 app.controller('menuCtrl', function ($scope, $state) {
     //toggle ECS menu
-    jQuery(".arrow-down").hide();
-    jQuery("#ecsMenu").click(function () {
-        jQuery("#ECSitem").slideToggle();
-        jQuery(this).find(".arrow-up, .arrow-down").toggle();
+    jQuery('.arrow-down').hide();
+    jQuery('#ecsMenu').click(function () {
+        jQuery('#ECSitem').slideToggle();
+        jQuery(this).find('.arrow-up, .arrow-down').toggle();
     });
-    jQuery(".icon-menu-hamburger").click(function () {
+    jQuery('.icon-menu-hamburger').click(function () {
         jQuery('.top-menu').addClass('open').slideDown();
         jQuery('.icon-menu-hamburger').css('visibility', 'hidden');
         jQuery('.icon-menu-close').css('visibility', 'visible');
     });
-    jQuery(".icon-menu-close").click(function () {
+    jQuery('.icon-menu-close').click(function () {
         jQuery('.top-menu').addClass('open').slideUp();
         jQuery('.icon-menu-hamburger').css('visibility', 'visible');
         jQuery('.icon-menu-close').css('visibility', 'hidden');
@@ -43,7 +58,7 @@ app.controller('menuCtrl', function ($scope, $state) {
 app.controller('loginCtrl', function ($scope, $state) {
     $scope.doLogin = function () {
         $state.go('main.custMgt');
-        //alert("haha");
+        //alert('haha');
     };
 });
 
